0412be3ca934d5fb51c88f46b00d8527fe03d6c0
Moorviper <moorviper@magdlos.com>
2015-10-27 00:37:28 +0100

    DEMO - Freifunk - API
    A Demo PDF

---
+++ Latex-Stuff/Demo-Freifunk-API/ff-api-update.php
@@ -0,0 +1,2 @@
+$NODELIST = 'https://meshviewer.fulda.freifunk.net/data/nodes.json';
+$APIFILE = '/var/www/fulda.freifunk.net/FreifunkFulda-api.json';
---
+++ Latex-Stuff/Demo-Freifunk-API/installation.sh
@@ -0,0 +1,8 @@
+# clone
+cd /opt
+git clone https://github.com/freifunk-fulda/ff-api-updater.git
+
+# cronjob
+cp /opt/ff-api-updater/ff-api-update.cron /etc/cron.d
+
+DONE !!!
